ce34c0b5e07d34c5ae322cdc0ab1bb8190a0bee1
bckd00r <48347237+bckd00r@users.noreply.github.com>
2021-06-11 01:21:32 +0300

    Python initialization :tada:

---
+++ GameplayScripts/map_awareness.py
@@ -0,0 +1,189 @@
+from winstealer import *
+
+winstealer_script_info = {
+    "script": "FOW",
+    "author": "bckd00r",
+    "description": "Cheat that improves your map awareness.",
+}
+
+bound_max = 0
+show_alert_enemy_close = False
+show_last_enemy_pos = False
+show_last_enemy_pos_minimap = False
+
+
+def winstealer_load_cfg(cfg):
+    global bound_max, show_alert_enemy_close, show_last_enemy_pos, show_last_enemy_pos_minimap
+    show_alert_enemy_close = cfg.get_bool("show_alert_enemy_close", True)
+    show_last_enemy_pos = cfg.get_bool("show_last_enemy_pos", True)
+    show_last_enemy_pos_minimap = cfg.get_bool("show_last_enemy_pos_minimap", True)
+    bound_max = cfg.get_float("bound_max", 4000)
+
+
+def winstealer_save_cfg(cfg):
+    global bound_max, show_alert_enemy_close, show_last_enemy_pos, show_last_enemy_pos_minimap
+    cfg.set_float("bound_max", bound_max)
+    cfg.set_bool("show_alert_enemy_close", show_alert_enemy_close)
+    cfg.set_bool("show_last_enemy_pos", show_last_enemy_pos)
+    cfg.set_bool("show_last_enemy_pos_minimap", show_last_enemy_pos_minimap)
+
+
+def winstealer_draw_settings(game, ui):
+    global bound_max, show_alert_enemy_close, show_last_enemy_pos, show_last_enemy_pos_minimap
+
+    show_last_enemy_pos = ui.checkbox(
+        "Show last position of champions", show_last_enemy_pos
+    )
+    show_last_enemy_pos_minimap = ui.checkbox(
+        "Show last position of champions on minimap", show_last_enemy_pos_minimap
+    )
+
+    ui.separator()
+    show_alert_enemy_close = ui.checkbox(
+        "Show champions that are getting close", show_alert_enemy_close
+    )
+    bound_max = ui.dragfloat(
+        "Alert when distance less than", bound_max, 100.0, 500.0, 10000.0
+    )
+
+
+def draw_champ_world_icon(
+    game,
+    champ,
+    pos,
+    size,
+    draw_distance=False,
+    draw_hp_bar=False,
+    draw_invisible_duration=False,
+):
+
+    ColorGray = Color.GRAY
+    ColorGray.a = 5.0
+    ColorGreen = Color.GREEN
+    ColorGreen.a = 5.0
+    ColorWhite = Color.WHITE
+    ColorWhite.a = 5.0
+
+    size_hp_bar = size / 10.0
+    percent_hp = champ.health / champ.max_health
+
+    # Draw champ icon
+    pos.x -= size / 2.0
+    pos.y -= size / 2.0
+    game.draw_image(
+        champ.name.lower() + "_square",
+        pos,
+        pos.add(Vec2(size, size)),
+        ColorWhite if champ.is_visible else ColorGray,
+    )
+
+    # Draw hp bar
+    if draw_hp_bar:
+        pos.y += size
+        game.draw_rect_filled(
+            Vec4(pos.x, pos.y, pos.x + size, pos.y + size_hp_bar), Color.BLACK
+        )
+        game.draw_rect_filled(
+            Vec4(
+                pos.x + 1,
+                pos.y + 1,
+                pos.x + 1 + (size - 1) * percent_hp,
+                pos.y + size_hp_bar - 1,
+            ),
+            ColorGreen,
+        )
+
+    # Draw distance
+    if draw_distance:
+        pos.x += size_hp_bar
+        pos.y += size_hp_bar
+        game.draw_text(
+            pos, "{:.0f}m".format(game.distance(champ, game.player)), ColorWhite
+        )
+
+    if not champ.is_visible and draw_invisible_duration:
+        # pos.x += 2*size_hp_bar
+        # pos.y += size_hp_bar
+        game.draw_text(
+            pos, "{:.0f}".format(game.time - champ.last_visible_at), ColorWhite
+        )
+
+
+def show_alert(game, champ):
+    if (
+        game.is_point_on_screen(champ.pos)
+        or not champ.is_alive
+        or not champ.is_visible
+        or champ.is_ally_to(game.player)
+    ):
+        return
+
+    pos = game.world_to_screen(
+        champ.pos.sub(game.player.pos).normalize().scale(500).add(game.player.pos)
+    )
+    draw_champ_world_icon(game, champ, pos, 50.0, True, True, True)
+
+
+def show_on_line(game, champ):
+    if (
+        not champ.is_alive
+        or not champ.is_visible
+        or champ.is_ally_to(game.player)
+        or game.is_point_on_screen(champ.pos)
+    ):
+        return
+
+    dist = champ.pos.distance(game.player.pos)
+    if dist > bound_max:
+        return
+
+    pos = game.world_to_screen(
+        champ.pos.sub(game.player.pos)
+        .normalize()
+        .scale(game.player.atk_range + game.player.gameplay_radius)
+        .add(game.player.pos)
+    )
+    game.draw_line(
+        game.world_to_screen(game.player.pos),
+        game.world_to_screen(champ.pos),
+        2,
+        Color.RED,
+    )
+    draw_champ_world_icon(game, champ, pos, 0.0, True, False, False)
+
+
+def show_last_pos_world(game, champ):
+    if (
+        champ.is_visible
+        or not champ.is_alive
+        or not game.is_point_on_screen(champ.pos)
+        or champ.is_ally_to(game.player)
+        and game.player.pos.distance(champ.pos)
+        < game.player.atkRange + game.player.gameplay_radius
+    ):
+        return
+    draw_champ_world_icon(
+        game, champ, game.world_to_screen(champ.pos), 36.0, False, True, True
+    )
+
+
+def show_last_pos_minimap(game, champ):
+    if champ.is_visible or not champ.is_alive or champ.is_ally_to(game.player):
+        return
+
+    draw_champ_world_icon(
+        game, champ, game.world_to_minimap(champ.pos), 24.0, False, True, True
+    )
+
+
+def winstealer_update(game, ui):
+    global bound_max, show_alert_enemy_close, show_last_enemy_pos, show_last_enemy_pos_minimap
+    for champ in game.champs:
+        if show_alert_enemy_close:
+            show_on_line(game, champ)
+
+        if show_last_enemy_pos:
+            show_last_pos_world(game, champ)
+
+        if show_last_enemy_pos_minimap:
+            show_last_pos_minimap(game, champ)
---
+++ GameplayScripts/object_viewer.py
@@ -0,0 +1,249 @@
+from winstealer import *
+from pprint import pprint
+
+winstealer_script_info = {
+    "script": "Object Tracker",
+    "author": "LView team + bckd00r",
+    "description": "object tracker",
+}
+
+
+def draw_spell(spell, ui):
+
+    if ui.treenode(str(spell.slot)):
+        ui.labeltext("name", spell.name)
+        ui.labeltext("missileName", spell.missileName)
+        ui.labeltext("summmoner_spell_type", str(spell.summoner_spell_type))
+        ui.dragint("level", spell.level)
+        ui.dragfloat("ready_at", spell.ready_at)
+        ui.dragfloat("value", spell.value)
+        ui.dragfloat("timeCharge", spell.timeCharge)
+
+        ui.separator()
+        ui.dragfloat("speed", spell.speed)
+        ui.dragfloat("cast_range", spell.cast_range)
+        ui.dragfloat("width", spell.width)
+        ui.dragfloat("cast_radius", spell.cast_radius)
+        ui.dragfloat("height", spell.height)
+        ui.dragfloat("delay", spell.delay)
+
+        ui.treepop()
+
+
+def draw_items(items, ui):
+    for item in items:
+        if ui.treenode(str(item.id)):
+
+            ui.dragint("slot", item.slot)
+            if item.movement_speed > 0:
+                ui.dragfloat("movement_speed", item.movement_speed)
+            if item.health > 0:
+                ui.dragfloat("health", item.health)
+            if item.crit > 0:
+                ui.dragfloat("crit", item.crit)
+            if item.ability_power > 0:
+                ui.dragfloat("ability_power", item.ability_power)
+            if item.mana > 0:
+                ui.dragfloat("mana", item.mana)
+            if item.armour > 0:
+                ui.dragfloat("armour", item.armour)
+            if item.magic_resist > 0:
+                ui.dragfloat("magic_resist", item.magic_resist)
+            if item.physical_damage > 0:
+                ui.dragfloat("physical_damage", item.physical_damage)
+            if item.attack_speed > 0:
+                ui.dragfloat("attack_speed", item.attack_speed)
+            if item.life_steal > 0:
+                ui.dragfloat("life_steal", item.life_steal)
+            if item.hp_regen > 0:
+                ui.dragfloat("hp_regen", item.hp_regen)
+            if item.movement_speed_percent > 0:
+                ui.dragfloat("movement_speed_percent", item.movement_speed_percent)
+
+            ui.treepop()
+
+
+def draw_buffs(buffs, ui):
+    for buff in buffs:
+        if ui.treenode(str(buff.name)):
+            ui.checkbox("isAlive", buff.isAlive)
+            if buff.count > 0:
+                ui.labeltext("count", str(buff.count))
+            if buff.countAlt > 0:
+                ui.labeltext("countAlt", str(buff.countAlt))
+            if buff.type > 0:
+                ui.dragint("type", buff.type)
+            if buff.start_time > 0:
+                ui.dragfloat("start_time", buff.start_time)
+            if buff.end_time > 0:
+                ui.dragfloat("end_time", buff.end_time)
+            ui.treepop()
+
+
+def draw_missile(obj, ui):
+    if ui.treenode(
+        "{}_({}->{}) ({})".format(obj.name, obj.src_id, obj.dest_id, hex(obj.address))
+    ):
+        ui.dragint("id", obj.id)
+        ui.labeltext("net_id", hex(obj.net_id))
+        ui.dragint("team", obj.team)
+
+        ui.labeltext(
+            "start_pos",
+            f"x={obj.start_pos.x:.2f}, y={obj.start_pos.y:.2f}, z={obj.start_pos.z:.2f}",
+        )
+        ui.labeltext(
+            "end_pos",
+            f"x={obj.end_pos.x:.2f}, y={obj.end_pos.y:.2f}, z={obj.end_pos.z:.2f}",
+        )
+        ui.labeltext("pos", f"x={obj.pos.x:.2f}, y={obj.pos.y:.2f}, z={obj.pos.z:.2f}")
+        ui.dragint("src_id", obj.src_id)
+        ui.dragint("dest_id", obj.dest_id)
+
+        ui.separator()
+        ui.dragfloat("speed", obj.speed)
+        ui.dragfloat("cast_range", obj.cast_range)
+        ui.dragfloat("width", obj.width)
+        ui.dragfloat("cast_radius", obj.cast_radius)
+        ui.dragfloat("height", obj.height)
+        ui.dragfloat("delay", obj.delay)
+        ui.treepop()
+
+
+def draw_game_object(obj, ui, additional_draw=None, set_open=False):
+
+    if obj == None:
+        ui.text("null", Color.RED)
+        return
+    if set_open:
+        ui.opennext()
+    if ui.treenode("{}_{} ({})".format(obj.name, obj.id, hex(obj.address))):
+        ui.labeltext("address", hex(obj.address))
+        ui.labeltext("net_id", hex(obj.net_id))
+        ui.labeltext("name", obj.name, Color.ORANGE)
+        ui.labeltext("pos", f"x={obj.pos.x:.2f}, y={obj.pos.y:.2f}, z={obj.pos.z:.2f}")
+        ui.dragint("id", obj.id)
+
+        ui.separator()
+        ui.dragfloat("health", obj.health)
+        ui.dragfloat("mana", obj.mana)
+        ui.dragfloat("max_mana", obj.max_mana)
+        ui.dragfloat("health_regen", obj.health_regen)
+        ui.dragfloat("mana_regen", obj.mana_regen)
+        ui.checkbox("is_alive", obj.is_alive)
+
+        ui.separator()
+        ui.dragfloat("currentDashSpeed", obj.currentDashSpeed)
+        ui.checkbox("isDashing", obj.isDashing)
+        ui.checkbox("dIsMoving", obj.dIsMoving)
+        ui.labeltext(
+            "dashPos",
+            f"x={obj.dashPos.x:.2f}, y={obj.dashPos.y:.2f}, z={obj.dashPos.z:.2f}",
+        )
+
+        ui.separator()
+        ui.dragfloat("base_atk", obj.base_atk)
+        ui.dragfloat("bonus_atk", obj.bonus_atk)
+        ui.dragfloat("armour", obj.armour)
+        ui.dragfloat("magic_resist", obj.magic_resist)
+        ui.dragfloat("ap", obj.ap)
+        ui.dragfloat("crit", obj.crit)
+        ui.dragfloat("crit_multi", obj.crit_multi)
+
+        ui.separator()
+        ui.dragfloat("atk_range", obj.atk_range)
+        ui.dragfloat("base_atk_range", obj.base_atk_range)
+        ui.dragfloat("base_atk_speed", obj.base_atk_speed)
+        ui.dragfloat("atk_speed_multi", obj.atk_speed_multi)
+        ui.dragfloat("atk_speed_ratio", obj.atk_speed_ratio)
+        ui.dragfloat("basic_missile_speed", obj.basic_missile_speed)
+        ui.dragfloat("base_ms", obj.base_ms)
+        ui.dragfloat("movement_speed", obj.movement_speed)
+
+        ui.separator()
+        ui.dragfloat("selection_radius", obj.selection_radius)
+        ui.dragfloat("gameplay_radius", obj.gameplay_radius)
+        ui.dragfloat("pathing_radius", obj.pathing_radius)
+        ui.dragfloat("acquisition_radius", obj.acquisition_radius)
+
+        ui.separator()
+        ui.dragfloat("duration", obj.duration)
+        ui.dragfloat("last_visible_at", obj.last_visible_at)
+
+        ui.text("Buffs")
+        draw_buffs(obj.buffs, ui)
+
+        ui.checkbox("is_visible", obj.is_visible)
+
+        if additional_draw != None:
+            additional_draw()
+
+        ui.treepop()
+
+
+def draw_champion(obj, ui):
+    def draw_spells():
+        ui.dragint("Level", obj.lvl)
+
+        ui.text("Items")
+        draw_items(obj.items, ui)
+
+        ui.text("Buffs")
+        draw_buffs(obj.buffs, ui)
+
+        ui.text("Skills")
+        draw_spell(obj.Q, ui)
+        draw_spell(obj.W, ui)
+        draw_spell(obj.E, ui)
+        draw_spell(obj.R, ui)
+        draw_spell(obj.D, ui)
+        draw_spell(obj.F, ui)
+
+    draw_game_object(obj, ui, additional_draw=draw_spells)
+
+
+def draw_list(label, objs, ui, draw_func):
+    if ui.treenode(label):
+        for obj in objs:
+            draw_func(obj, ui)
+        ui.treepop()
+
+
+def winstealer_load_cfg(cfg):
+    pass
+
+
+def winstealer_save_cfg(cfg):
+    pass
+
+
+def winstealer_draw_settings(objs, ui):
+    pass
+
+
+def winstealer_update(game, ui):
+
+    ui.begin("Object Viewer")
+
+    ui.dragfloat("time", game.time)
+    ui.dragfloat("chat", game.isChatOpen)
+    ui.dragint("ping", game.ping)
+    if game.hovered_obj:
+        ui.labeltext(
+            "hovered_obj", f"{game.hovered_obj.name} ({hex(game.hovered_obj.address)})"
+        )
+    else:
+        ui.labeltext("hovered_obj", "none")
+
+    ui.text("Local Champion")
+    draw_champion(game.player, ui)
+
+    ui.text("Lists")
+    draw_list("Champions", game.champs, ui, draw_champion)
+    draw_list("Minions", game.minions, ui, draw_game_object)
+    draw_list("Jungle", game.jungle, ui, draw_game_object)
+    draw_list("Turrets", game.turrets, ui, draw_game_object)
+    draw_list("Missiles", game.missiles, ui, draw_missile)
+    draw_list("Others", game.others, ui, draw_game_object)
+
+    ui.end()
